3420b6cc1fc6adf3730506ca7faf3ec8479af533
Henry <henryquoctran@gmail.com>
2014-01-26 11:55:12 -0800

    title and haribo art

--- entities/titlescreen.lua
+++ entities/titlescreen.lua
@@ -7,6 +7,8 @@ function TitleScreen:initialize()
     PhysBox.initialize(self)
     singleCamera = true
     self.isCamera = true
+
+    self.image = love.graphics.newImage("assets/sprites/bako.png")
 end
 
 function TitleScreen:initPhysics()
@@ -28,6 +30,9 @@ function TitleScreen:update(dt)
 end
 
 function TitleScreen:draw()
+    local x, y = self:getPosition()
+    love.graphics.draw(self.image, x-self.image:getWidth()/2, y-self.image:getHeight()/2)
+    -- love.graphics.draw(self.image, -self.image:getWidth()/2, -self.image:getHeight()/2)
 end
 
 return TitleScreen
