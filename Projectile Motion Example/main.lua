2f89b40ea5b988f66dbd65216e2ddfa0200ea0fb
Gavin Higham <Gavin.Higham@gmail.com>
2014-02-03 04:27:02 -0800

    Much better projectile code.
    Much better projectile code. This will allow the power of the
    trampoline to be set independent of the target location. That means
    better range, and a choice of higher/lower arcs for any given launch.

--- Projectile Motion Example/main.lua
+++ Projectile Motion Example/main.lua
@@ -1,31 +1,47 @@
-gravity = 1000
+local gravity = 300
 local xSpawnPos = love.graphics.getWidth() / 4
 local ySpawnPos = love.graphics.getHeight() / 2
 local liveEntities = {}
+local vel = 400 -- Higher vel gives higher apex and more range. Lower clears low ceilings.
 
 function newLiveCircle(x, y)
-	dx = x - xSpawnPos
-	dy = y - ySpawnPos
-	print("dx: " .. dx .. " dy: " .. dy)
-	local vel = math.sqrt((math.pow(dx, 2)*gravity)/(2*(dx+dy)))
-	print(vel)
-	if vel == vel then
-		local body = love.physics.newBody(world, xSpawnPos, ySpawnPos, 'dynamic')
-		local shape = love.physics.newCircleShape(10)
-		local density = 1
-		local fixture = love.physics.newFixture(body, shape, density)
-		body:setLinearVelocity(vel, -vel)
-		table.insert(liveEntities, body)
+	local dx = x - xSpawnPos
+	local dy = ySpawnPos - y -- Account for funky coordinate systems.
+	local theta, vx, vy -- vx and vy are the x and y components of the projectile velocity.
+
+	-- Root is [÷/-]1. Sometimes two parabolas are possible, root selects which one is to be used.
+	-- root = -1 will launch the projectile such that it hits the target before the apex, if possible.
+	local root = 1
+	-- The discriminant determines if the shot is even possible.
+	local discriminant = math.pow(vel, 4) - gravity*(gravity*dx*dx + 2*dy*vel*vel)
+	if discriminant > 0 then
+		-- Find the angle of launch.
+		theta = math.atan((vel*vel + root*math.sqrt(discriminant))/(gravity*dx))
+		-- Divide the velocity into x and y components.
+		vx = vel*math.cos(theta)
+		vy = vel*math.sin(theta)
+		print("theta: " .. math.deg(theta) .. " vx: ".. vx .. " vy: " .. vy)
+	
+		-- Sanity check to avoid feeding a nil value into the physics engine.
+		if vx == vx and vy == vy then
+			-- Actually create and fire the projectile.
+			print("FIRING!")
+			local body = love.physics.newBody(world, xSpawnPos, ySpawnPos, 'dynamic')
+			local shape = love.physics.newCircleShape(10)
+			local density = 1
+			local fixture = love.physics.newFixture(body, shape, density)
+			body:setLinearVelocity(vx, -vy)
+			table.insert(liveEntities, body)
+		end
 	end
 end
 
 function love.mousepressed(x, y, button)
-	print("Mouse X: " .. x .. "Mouse Y:" .. y)
+	print("Mouse X: " .. x .. " Mouse Y:" .. y)
 	newLiveCircle(x, y)
 end
 
 function love.load()
-	love.physics.setMeter(32)
 	world = love.physics.newWorld()
 	world:setGravity(0, gravity)
 end
