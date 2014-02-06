1906390d570e908040ad2d805ec6d3e8c19d7d42
Gavin Higham <Gavin.Higham@gmail.com>
2014-02-06 13:45:24 -0800

    Made it fullscreen, now it wants me to commit.

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.identity = "bakobakobako"
     t.window.title = "BAKO IKIMASHOU!"
-    -- t.window.fullscreen = true
-    -- t.window.width = 1280
-    -- t.window.height = 800
+    t.window.fullscreen = true
+    t.window.width = 1280
+    t.window.height = 800
 end
